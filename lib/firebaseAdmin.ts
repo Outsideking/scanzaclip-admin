2398baae43ccf79dfd5ddf6e561a35f62914e152
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-08 00:58:52 +0700

    Update firebaseAdmin.ts

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,23 +1,10 @@
-
-import admin from 'firebase-admin';
-
-const serviceAccount = {
-  "type": "service_account",
-  "project_id": "scanzaclip",
-  "private_key_id": "your_private_key_id",
-  "private_key": "-----BEGIN PRIVATE KEY-----\\nYOUR_PRIVATE_KEY_HERE\\n-----END PRIVATE KEY-----\\n",
-  "client_email": "firebase-adminsdk-xxx@scanzaclip.iam.gserviceaccount.com",
-  "client_id": "your_client_id",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "your_client_cert_url"
-};
+import * as admin from 'firebase-admin';
+import serviceAccount from './serviceAccountKey.json'; // ✅ ใช้ import JSON
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
   });
 }
 
-export default admin;
+export default admin.firestore();

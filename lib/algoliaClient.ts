5cc031823070df0d6cd92ba8e832aa030b8c04fd
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-10 09:17:57 +0700

    Update algoliaClient.ts

--- lib/algoliaClient.ts
+++ lib/algoliaClient.ts
@@ -1,1 +1,11 @@
+import algoliasearch from "algoliasearch";
+
+const client = algoliasearch(
+  process.env.NEXT_PUBLIC_ALGOLIA_APP_ID || "",
+  process.env.ALGOLIA_ADMIN_API_KEY || ""
+);
+
+const index = client.initIndex("scripts");
+
+export { client, index };
 
